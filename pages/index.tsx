e0ee239158f32c55c0fe22c65ebe45b663386ee2
joe-main <qmmplusfact@gmail.com>
2022-05-29 12:26:02 +0900

    refactoring for chapter_2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
+import React from "react";
 import { useState } from "react";
 
-const Child = () => {
+const Child = React.memo(() => {
   console.log("子が再レンダリング");
 
   return (
-    <>
-      <p>子コンポーネントが表示されています</p>
-    </>
+      <>
+        <p>子コンポーネントが表示されています</p>
+      </>
   );
-};
+});
+
+// ①esLintエラー対策
+Child.displayName = 'Child';
 
 export default function Parent() {
   console.log("親が再レンダリング!");
@@ -19,10 +23,14 @@ export default function Parent() {
   };
 
   return (
-    <>
+// ②Reactフラグメントを削除してdivタグを追加
+    <div>
       <p>親コンポーネントで文字を入力</p>
       <input type="text" onChange={changeText} />
       <Child />
-    </>
+    </div>
   );
 }
+
+// whyDidYouRenderで利用します
+Child.whyDidYouRender = true
